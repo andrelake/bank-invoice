d9ea2bf5b1106d4094e998634c88e8d8ed84bce0
andreLake <andre.lake@hotmail.com>
2025-03-05 16:13:17 -0300

    started

---
+++ src/main/java/com/andrelake/bank_invoice/BankInvoiceApplication.java
@@ -0,0 +1,13 @@
+package com.andrelake.bank_invoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankInvoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankInvoiceApplication.class, args);
+	}
+
+}
