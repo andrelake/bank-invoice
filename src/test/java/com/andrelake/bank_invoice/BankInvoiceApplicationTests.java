0f65de4f48e9a3793d539bdc8b27ef6e8eba352f
andreLake <andre.lake@hotmail.com>
2025-03-05 18:16:47 -0300

    add file and create tests

--- src/test/java/com/andrelake/bank_invoice/BankInvoiceApplicationTests.java
+++ src/test/java/com/andrelake/bank_invoice/BankInvoiceApplicationTests.java
@@ -1,13 +1,56 @@
 package com.andrelake.bank_invoice;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.DecimalFormat;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BankInvoiceApplicationTests {
 
-	@Test
-	void contextLoads() {
+	DecimalFormat df;
+
+	String path;
+	List<String> lines;
+
+	@BeforeEach
+	void setUp() throws IOException {
+		path = Paths.get("src/main/resources/static/extrato_fake.csv").toAbsolutePath().toString();
+		lines = Files.readAllLines(Paths.get(path));
+		df = new DecimalFormat("####0.00");  
 	}
+
+	@Test
+	void sumTransactions() throws IOException {
+		var sum = lines.stream()
+		.mapToDouble(line -> Double.parseDouble(line.split(",")[1]))
+		.reduce(0.0, Double::sum);
+		
+		System.out.println(df.format(sum));
+	}		
+	
+	@Test
+	void countTransactionsByMonth() throws IOException {
+		var dateFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+		var total = 0.0;
+
+		for (String line : lines) {
+			var date = LocalDate.parse(line.split(",")[0], dateFormatter);
+			if(date.getMonth() == Month.JANUARY) {
+				var amount = Double.parseDouble(line.split(",")[1]);
+				total += amount;
+			}
+		}
+
+		System.out.println(df.format(total));
+	}	
 
 }
